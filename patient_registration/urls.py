bf9c4bf826e0bbbd4b421bd947b2f6bc63527f64
Naveen Bhardwaj <naveen6252@gmail.com>
2020-01-13 01:58:35 +0530

    Added the routes and finished the base template

--- patient_registration/urls.py
+++ patient_registration/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='patient-registration-home'),
+    path('add-patient/', views.add_patient, name='patient-registration-add-patient'),
+    path('add-service/', views.add_service, name='patient-registration-add-service'),
+    path('add-appointment/', views.add_appointment, name='patient-registration-add-appointment'),
+    path('appointments/', views.appointment, name='patient-registration-appointments'),
+    path('invoice/', views.invoice, name='patient-registration-invoice'),
 ]
--- patient_registration/views.py
+++ patient_registration/views.py
@@ -4,4 +4,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'patient_registration/home.html')
+    return render(request, 'patient_registration/dashboard.html', {'title': 'Dashboard'})
+
+
+def add_patient(request):
+    return render(request, 'patient_registration/add-patient.html', {'title': 'Add'})
+
+
+def add_service(request):
+    return render(request, 'patient_registration/add-service.html', {'title': 'Add'})
+
+
+def add_appointment(request):
+    return render(request, 'patient_registration/add-appointment.html', {'title': 'Add'})
+
+
+def appointment(request):
+    return render(request, 'patient_registration/appointments.html', {'title': 'Appointments'})
+
+
+def invoice(request):
+    return render(request, 'patient_registration/invoice.html', {'title': 'Invoice'})
