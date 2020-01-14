d100aab7f7461baf4f122d0d4a06718eb0d9ddfa
Naveen Bhardwaj <naveen6252@gmail.com>
2020-01-14 12:04:39 +0530

    Added the models and relationships

--- patient_registration/admin.py
+++ patient_registration/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Doctor, Patient, Service, Invoice, Appointment
 
 # Register your models here.
+admin.site.register([Doctor, Patient, Service, Invoice, Appointment])
--- patient_registration/models.py
+++ patient_registration/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    contact = models.CharField(max_length=15)
+    designation = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(models.Model):
+    gender_choices = (('M', 'Male'), ('F', 'Female'), ('O', 'Other'))
+    patient_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=15)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=1, choices=gender_choices)
+    address = models.TextField()
+
+    def __str__(self):
+        return str(self.patient_id)
+
+
+class Invoice(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    total_amount = models.IntegerField(default=0)
+    deposit = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Service(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    treatment = models.CharField(max_length=50)
+    tooth = models.CharField(max_length=50, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+    laboratory_name = models.CharField(max_length=50, blank=True)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.treatment
+
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return '{0} {1}'.format(self.doctor, self.date)
