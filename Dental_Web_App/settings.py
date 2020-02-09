c9233d501db5ac0df2eeca6f1fffd3982ff2f99d
Naveen Bhardwaj <naveen6252@gmail.com>
2020-02-10 02:29:33 +0530

    heroku upload

--- Dental_Web_App/settings.py
+++ Dental_Web_App/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&3(mae)02yds-wdw10%54))&q@hd7#yaf%)#q5nf4y)-h_s7$='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['harbor-dental.herokuapp.com']
 
